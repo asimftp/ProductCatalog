1f99eb7075faaae0beefc2ca20d74264f7f9b5b1
Mohd Asim <169433958+asimftp@users.noreply.github.com>
2025-09-13 21:58:26 +0530

    Commit changes for 1. rename svg 2. fix css for home page 3. add waiting messege while server respond late 4. fix the nav bar option not shown on mobile 5. add error handling in get product method

--- ClientApp/src/Pages/Home.tsx
+++ ClientApp/src/Pages/Home.tsx
@@ -7,16 +7,22 @@ import { ProductCard } from '../components/ProductCard';
 export const Home = () => {
     const [products, setProducts] = useState<Product[]>([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
 
     const loadProducts = async () => {
         try {
             setLoading(true);
             const data = await getProducts();
-            if(data){
-            setProducts(data);
+            if (data) {
+                setProducts(data);
             }
         } catch (error) {
             console.error("Failed to fetch products:", error);
+            setError("Getting things ready for you… just a moment.");
+            setTimeout(() => {
+                setError(null);
+                console.clear();
+            }, 50000);
         } finally {
             setLoading(false);
         }
@@ -26,25 +32,44 @@ export const Home = () => {
         loadProducts();
     }, []);
 
+    const CenteredMessage = ({ message }: { message: string }) => {
+        return (
+            <div className="d-flex justify-content-center align-items-center" style={{ height: '85vh' }}>
+                <div className="spinner-border" role="status">
+                    <span className="sr-only"></span>
+                </div>
+                <span className="">&nbsp;&nbsp;{message}</span>
+            </div>
+        );
+    };
     if (loading) {
-        return <div className="container py-4">Loading products...</div>;
+        return <CenteredMessage message="" />;
+    }
+    if (error) {
+        return (
+            <CenteredMessage message={error} />
+        );
     }
 
     return (
         <div className="container py-4">
-            <div className="d-flex flex-wrap gap-4">
-                {products.length > 0 ? (
-                    products.map((p) => (
+            {products.length > 0 ? (
+                <div className="d-flex flex-wrap gap-4">
+                    {products.map((p) => (
                         <ProductCard
                             key={p.productId}
                             product={p}
                             onDelete={loadProducts}   // refresh product list after deletion
                         />
-                    ))
-                ) : (
-                    <div>No products found.</div>
-                )}
-            </div>
+                    ))}
+                </div>
+            ) : (
+                <div
+                    className="d-flex justify-content-center align-items-center text-muted fs-4"
+                    style={{ height: '85vh' }}>
+                    No products found.
+                </div>
+            )}
         </div>
     );
 };
